ba24a19927e627ba0ec391e87467ce228dc2cca8
alex.kot.1993@internet.ru <alex.kot.1993@internet.ru>
2021-10-15 14:14:52 +0300

    Тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину.

--- test_items.py
+++ test_items.py
@@ -1,20 +1,14 @@
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-
-class TestProductPage:
-    
-    def test_button_add_to_basket_is_visible(self, browser):
-        """Тест проверяет, что страница товара
-         содержит кнопку добавления в корзину
-        """
-        # Открываем страницу товара
-        browser.get(link)
-        
-        # Установлено время принудительной задержки браузера
-        # после открытия страницы, для визуальной проверки языка открытой страницы
-        time.sleep(5)
-        
-        # Проверяем наличие кнопки добавления товара в корзину
-        assert browser.find_element_by_css_selector("button.btn-add-to-basket")
+def test_exist_add_to_cart_button(browser):
+	# переход на нужную страницу
+    browser.get(link)
+    time.sleep(30)
+	# поиск кнопки добавления товара в корзину
+    WebDriverWait(browser, 3).until(
+		EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "No add to cart button on page")
